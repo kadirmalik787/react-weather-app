9873ff6209105167bd480e16f83977dc7ae4b6d4
kadirmalik787 <kadirmalik457@gmail.com>
2023-03-21 06:39:23 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Temp from "./components/Temp";
+
+function App() {
+  return (
+   <>
+   {/* f2cc6900fc6c378b307a0fd169f1a0cb;
+   
+  api.openweathermap.org/data/2.5/weather?q=pune&appid=59eea87ae568c085996039c042a6a369 */}
+  <Temp/>
+
+   <div></div>
+   </>
+  );
+}
+
+export default App;
